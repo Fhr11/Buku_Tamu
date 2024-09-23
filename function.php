157436b21aa799c53bb491df29fc62fa40418d8e
Fhr11 <official.fahri09u@gmail.com>
2024-09-23 20:33:03 +0700

    menambahkan halaman hapus tamu dan fitur hapus data tamu

--- function.php
+++ function.php
@@ -3,12 +3,67 @@
 require_once('koneksi.php');
 
 //membuat query ke / dari database
-function query($query){
+function query($query)
+{
     global $koneksi;
     $result = mysqli_query($koneksi, $query);
     $rows = [];
-    while($row = mysqli_fetch_assoc($result)){
+    while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
     return $rows;
+}
+
+//function tambah data
+function tambah_tamu($data)
+{
+    global $koneksi;
+
+    $kode           = htmlspecialchars($data["nama_tamu"]);
+    $tanggal        = date("Y-m-d");
+    $nama_tamu      = htmlspecialchars($data["nama_tamu"]);
+    $alamat         = htmlspecialchars($data["alamat"]);
+    $no_hp          = htmlspecialchars($data["no_hp"]);
+    $bertemu        = htmlspecialchars($data["bertemu"]);
+    $kepentingan    = htmlspecialchars($data["kepentingan"]);
+
+    $query = "INSERT INTO buku_tamu VALUES ('$kode','$tanggal','$nama_tamu','$alamat', '$no_hp','$bertemu','$kepentingan')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+//function ubah data tamu
+function ubah_tamu($data)
+{
+    global $koneksi;
+
+    $id = htmlspecialchars($data["id_tamu"]);
+    $nama_tamu = htmlspecialchars($data["nama_tamu"]);
+    $alamat = htmlspecialchars($data["alamat"]);
+    $no_hp = htmlspecialchars($data["no_hp"]);
+    $bertemu = htmlspecialchars($data["bertemu"]);
+    $kepentingan = htmlspecialchars($data["kepentingan"]);
+
+    $query = "UPDATE buku_tamu SET
+                nama_tamu = '$nama_tamu',
+                alamat = '$alamat',
+                no_hp = '$no_hp',
+                bertemu = '$bertemu',
+                kepentingan = '$kepentingan'
+                WHERE id_tamu = $id
+                ";
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+//function hapus data tamu
+function hapus_tamu($id)
+{
+    global $koneksi;
+    $query="DELETE FROM buku_tamu WHERE id_tamu = '$id'";
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
 }
---
+++ hapus-tamu.php
@@ -0,0 +1,19 @@
+<?php
+    //panggil file function.php
+    require_once('function.php');
+
+    //jika ada id
+    if (isset($_GET['id_tamu'])) {
+        $id_tamu = $_GET['id_tamu'];
+        if (hapus_tamu($id_tamu) > 0) {
+            //jika data breehasil di hapus maka akan muncul alert
+            echo "<script>alert('Data berhasil dihapus')<script>";
+            //redirect ke halaman buku-tamu.php
+            echo "<script>window.location='buku-tamu.php'</script>";
+        } else {
+            //jika gagagl dihapus
+            echo "<script>alert('Data gagal dihapus')<script>";
+        }
+    }
+
+    ?>
